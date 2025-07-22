fcc1c7a7975e4a4d9d7463ea9073047266c9563b
xiexin12138 <xiexin344@163.com>
2025-07-22 13:40:48 +0800

    fix: 修改错误命名，新增类型定义

---
+++ types/index.ts
@@ -0,0 +1,12 @@
+export interface User {
+  id: string;
+  name?: string;
+  email?: string;
+  username?: string;
+  data: Record<string, unknown>;
+}
+
+export interface AuthResponse {
+  user: User | null;
+  isLoggedIn: boolean;
+}
