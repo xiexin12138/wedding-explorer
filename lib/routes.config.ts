0c0e8c984412084a3384c5ee1dfc9d2e7f3be054
xiexin12138 <xiexin344@163.com>
2025-07-31 12:01:34 +0800

    fix: 临时调试放开

--- lib/routes.config.ts
+++ lib/routes.config.ts
@@ -12,6 +12,7 @@ export const PUBLIC_ROUTES = [
   '/login',      // 登录页
   '/callback',    // 登录回调页
   '/images',
+  '/map', // todo: 临时调试放开
   '/api/auth/set-session', // 设置会话接口（登录过程中调用）
   '/api/auth/check',       // 检查认证状态接口（需要在未认证时也能访问）
   '/api/auth/logout',      // 登出接口（需要在任何时候都能访问）
